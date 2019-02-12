d0c7c3df6f408a15426d00677d062220c8bce8b4
Johnathan <jonathan_dall75@hotmail.com>
2019-02-13 00:45:24 +0100

    Deleted .Net Core Razor boilerplate and added .Net Core MVC

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace schoolProject
+namespace TicketMVC
 {
     public class Program
     {
